2ab91951934ab3143a396c9925ecb24e943decc1
lixun1994 <1261142605@qq.com>
2017-06-28 18:06:02 +0800

    commit

--- RNDemo1/listViewClass.js
+++ RNDemo1/listViewClass.js
@@ -6,16 +6,19 @@ export default class listViewClass extends Component{
     constructor(props){
         super(props);
           var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-
+          
           this.state = {
-              dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+              dataSource: ds.cloneWithRows(['row1','row2','row3','row4']),
           };
     }
 
     render(){
         return(
-            <View style={{flex:1}}>
-                <ListView dataSource={this.state.dataSource} renderRow={this._renderRow.bind(this)}>
+            <View style={{flex:1,marginTop:20}}>
+                <ListView 
+                dataSource={this.state.dataSource} 
+                renderRow={this._renderRow.bind(this)} 
+                renderSeparator={this._renderSeparator.bind(this)}>
                 </ListView>
             </View>
         );
@@ -25,10 +28,17 @@ export default class listViewClass extends Component{
     _renderRow(rowData, sectionID, rowID, highlightRow){
         console.log(rowData,sectionID,rowID,highlightRow);
         return(
-            <View>
-                <Text>{rowData}</Text>
+            <View style={{flex:1,justifyContent:'center',height:50,alignItems:'center'}}>
+                <Text style={{fontSize:15}}>{rowData}</Text>
             </View>
         );
+    }
+
+     _renderSeparator(sectionID, rowID, adjacentRowHighlighted)  {
+        console.log(sectionID,rowID,adjacentRowHighlighted);
+        return (
+            <View style={{height:1,backgroundColor:'black'}}></View>
+        )
     }
 
 
