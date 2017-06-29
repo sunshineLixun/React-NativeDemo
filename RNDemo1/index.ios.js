faf509adc368cdae456b74791f0562ee2ab68b1c
lixun1994 <1261142605@qq.com>
2017-06-29 10:05:47 +0800

    整理项目结构

--- RNDemo1/index.ios.js
+++ RNDemo1/index.ios.js
@@ -2,9 +2,9 @@
 import React, {Component} from 'react';
 import { AppRegistry, View, Text, StyleSheet } from 'react-native';
 
-import setLayout from './setLayout';
-import setProps from './setProps'
-import componentByValue from './componentByValue'
-import listViewClass from './listViewClass'
+import setLayout from './src/components/setLayout';
+import setProps from './src/components/setProps'
+import componentByValue from './src/components/componentByValue'
+import listViewClass from './src/components/listViewClass'
 
 AppRegistry.registerComponent('RNDemo1', () => listViewClass);
